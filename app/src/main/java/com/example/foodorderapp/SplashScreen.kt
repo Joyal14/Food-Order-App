6bcfdfe64826266925379107d271d15e0be30f4a
Joyal14 <dsilvajoyal@gmail.com>
2023-07-24 08:39:56 +0530

    Splash screen and start page done

---
+++ app/src/main/java/com/example/foodorderapp/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.example.foodorderapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed(3500) {
+            startActivity(Intent(this, StartActivity::class.java))
+            finish()
+        }
+    }
+}
